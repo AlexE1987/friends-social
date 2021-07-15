2fce5a2683db0641b3ebf9f9006d5ae5f1e4ce8a
AlexE1987 <evsikovad1987@gmail.com>
2021-07-15 19:08:43 +0400

    refactor: add dispatch in store

--- src/index.js
+++ src/index.js
@@ -1,21 +1,17 @@
 // import state, { subscribe } from './redux/state';
+import './index.css';
+import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 // import { addPost, updateNewPostText } from './redux/state';
 import store from './redux/state';
 
 let rerenderEntireTree = (state) => {
   ReactDOM.render(
     <BrowserRouter>
-      <App
-        state={store.getState()}
-        addPost={store.addPost.bind(store)}
-        updateNewPostText={store.updateNewPostText}
-      />
+      <App state={state} dispatch={store.dispatch.bind(store)} />
     </BrowserRouter>,
     document.getElementById('root'),
   );
