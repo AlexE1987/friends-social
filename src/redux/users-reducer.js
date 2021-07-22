2afe9b9a80c5f387b8bc6097f8c670f0b9b3232c
AlexE1987 <evsikovad1987@gmail.com>
2021-07-22 17:15:37 +0400

    feat: pagination in Users

--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -1,34 +1,14 @@
 const FOLLOW = 'FOLLOW';
 const UNFOLLOW = 'UNFOLLOW';
 const SET_USERS = 'SET_USERS';
+const SET_CURRENT_PAGE = 'SET_CURRENT_PAGE';
+const SET_TOTAL_USERS_COUNT = 'SET_TOTAL_USERS_COUNT';
 
 let initialState = {
-  users: [
-    //   {
-    //     id: 1,
-    //     photoUrl: 'https://i.pinimg.com/originals/1b/fc/02/1bfc0220938a4050c06ad363dc28657f.png',
-    //     followed: true,
-    //     fullName: 'Alex',
-    //     status: 'Winner',
-    //     location: { city: 'Saratov', country: 'Russia' },
-    //   },
-    //   {
-    //     id: 2,
-    //     photoUrl: 'https://i.pinimg.com/originals/1b/fc/02/1bfc0220938a4050c06ad363dc28657f.png',
-    //     followed: false,
-    //     fullName: 'Max',
-    //     status: 'Winner',
-    //     location: { city: 'Minsk', country: 'Belarus' },
-    //   },
-    //   {
-    //     id: 3,
-    //     photoUrl: 'https://i.pinimg.com/originals/1b/fc/02/1bfc0220938a4050c06ad363dc28657f.png',
-    //     followed: true,
-    //     fullName: 'Den',
-    //     status: 'Winner',
-    //     location: { city: 'Kiev', country: 'Ukraine' },
-    //   },
-  ],
+  users: [],
+  pageSize: 5,
+  totalUsersCount: 0,
+  currentPage: 1,
 };
 
 const usersReducer = (state = initialState, action) => {
@@ -56,7 +36,13 @@ const usersReducer = (state = initialState, action) => {
       };
     }
     case SET_USERS: {
-      return { ...state, users: [...state.users, ...action.users] };
+      return { ...state, users: action.users };
+    }
+    case SET_CURRENT_PAGE: {
+      return { ...state, currentPage: action.currentPage };
+    }
+    case SET_TOTAL_USERS_COUNT: {
+      return { ...state, totalUsersCount: action.count };
     }
     default:
       return state;
@@ -66,5 +52,10 @@ const usersReducer = (state = initialState, action) => {
 export const followAC = (userId) => ({ type: FOLLOW, userId });
 export const unfollowAC = (userId) => ({ type: UNFOLLOW, userId });
 export const setUsersAC = (users) => ({ type: SET_USERS, users });
+export const setCurrentPageAC = (currentPage) => ({ type: SET_CURRENT_PAGE, currentPage });
+export const setTotalUsersCountAC = (totalUsersCount) => ({
+  type: SET_TOTAL_USERS_COUNT,
+  count: totalUsersCount,
+});
 
 export default usersReducer;
