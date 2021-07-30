ee8fc3654a50a23cd640e86ec971274392a35b87
AlexE1987 <evsikovad1987@gmail.com>
2021-07-30 22:34:51 +0400

    refactor: add ProfileStatusWithHooks

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,6 +1,7 @@
 import s from './ProfileInfo.module.css';
 import Preloader from "../../common/Preloader/Preloader";
 import ProfileStatus from './ProfileStatus';
+import ProfileStatusWithHooks from "./ProfileStatusWithHooks";
 
 const ProfileInfo = (props) => {
 	if (!props.profile) {
@@ -16,7 +17,7 @@ const ProfileInfo = (props) => {
 				<img src={props.profile.photos.large} alt=""/>
 				ava + desc
 				<div>
-					<ProfileStatus status={props.status} updateStatus={props.updateStatus}/>
+					<ProfileStatusWithHooks status={props.status} updateStatus={props.updateStatus}/>
 				</div>
 			</div>
 		</div>
