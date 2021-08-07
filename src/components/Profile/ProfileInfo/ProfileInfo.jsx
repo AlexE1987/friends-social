fc8ba06f9e5684cb2ace9429697c3771c96db4cc
AlexE1987 <evsikovad1987@gmail.com>
2021-08-07 16:35:27 +0400

    feat: add profileData and profileDataForm

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,24 +1,71 @@
 import s from './ProfileInfo.module.css';
 import Preloader from "../../common/Preloader/Preloader";
 import ProfileStatusWithHooks from "./ProfileStatusWithHooks";
+import userPhoto from '../../../assets/images/userPhoto.ico'
+import Profile from "../Profile";
+import {useState} from "react";
+import ProfileDataForm from "./ProfileDataForm";
 
-const ProfileInfo = ({profile, status, updateStatus}) => {
+
+const ProfileInfo = ({profile, status, updateStatus, isOwner, savePhoto}) => {
+
+	let [editMode, setEditMode] = useState(false);
+	// let [status, setStatus] = useState(status);
+
 	if (!profile) {
 		return <Preloader/>
 	}
+	const onMainPhotoSelect = (e) => {
+		if (e.target.files.length) {
+			savePhoto(e.target.files[0]);
+		}
 
+	}
 	return (
 		<div className="">
 			<div className={s.descriptionBlock}>
-				<img src={profile.photos.large} alt=""/>
-				ava + desc
-				<div>
-					<ProfileStatusWithHooks status={status} updateStatus={updateStatus}/>
-				</div>
+				<img src={profile.photos.large || userPhoto} alt="Avatar" className={s.namePhoto}/>
+				{isOwner && <input type={'file'} onChange={onMainPhotoSelect}/>}
+				{editMode
+					? <ProfileDataForm profile={profile}/>
+					: <ProfileData profile={profile} isOwner={isOwner} goToEditMode={() => setEditMode(true)}/>}
+				<ProfileStatusWithHooks status={status} updateStatus={updateStatus}/>
 			</div>
 		</div>
 	);
 };
 
+const ProfileData = ({profile, isOwner, goToEditMode}) => {
+	return (
+		<div>
+			{isOwner && <div>
+				<button onClick={goToEditMode}>edit</button>
+			</div>}
+			<div>
+				<b>Full name</b>: {profile.fullName}
+			</div>
+			<div>
+				<b>Looking for a job</b>: {profile.lookingForAJob ? 'yes' : 'no'}
+			</div>
+			<div>
+				<b>My professional skills</b>: {profile.lookingForAJobDescription}
+			</div>
+			<div>
+				<b>About me</b>: {profile.aboutMe}
+			</div>
+			<div>
+				<b>Contacts</b>: {Object.keys(profile.contacts).map(key => {
+				return <Contact key={key} contactTitle={key} contactValue={profile.contacts[key]}/>
+			})
+			}
+			</div>
+		</div>
+	)
+}
+
+
+const Contact = ({contactTitle, contactValue}) => {
+	return <div className={s.contact}><b>{contactTitle}</b><b>{contactValue}</b></div>
+}
 
 export default ProfileInfo;
--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -4,6 +4,7 @@ const ADD_POST = 'ADD-POST';
 const SET_USER_PROFILE = 'SET_USER_PROFILE';
 const SET_STATUS = 'SET_STATUS';
 const DELETE_STATUS = 'DELETE_STATUS';
+const SAVE_PHOTO_SUCCESS = 'SAVE_PHOTO_SUCCESS';
 
 
 let initialState = {
@@ -39,11 +40,16 @@ const profileReducer = (state = initialState, action) => {
 				status: action.status,
 			};
 		}
-
 		case DELETE_STATUS: {
 			return {
 				...state,
 				posts: state.posts.filter(p => p.id != action.postId)
+			};
+		}
+		case SAVE_PHOTO_SUCCESS: {
+			return {
+				...state,
+				profile: {...state.profile, photos: action.photos},
 			};
 		}
 
@@ -56,6 +62,7 @@ export const addPostActionCreator = (newPostText) => ({type: ADD_POST, newPostTe
 export const setUserProfile = (profile) => ({type: SET_USER_PROFILE, profile});
 export const setStatus = (status) => ({type: SET_STATUS, status});
 export const deletePost = (postId) => ({type: DELETE_STATUS, postId});
+export const savePhotoSuccess = (photos) => ({type: SAVE_PHOTO_SUCCESS, photos});
 
 export const getUserProfile = (userId) => {
 	return async (dispatch) => {
@@ -76,6 +83,15 @@ export const updateStatus = (status) => {
 		let response = await profileAPI.updateStatus(status)
 		if (response.data.resultCode === 0) {
 			dispatch(setStatus(status));
+		}
+	}
+};
+
+export const savePhoto = (file) => {
+	return async (dispatch) => {
+		let response = await profileAPI.savePhoto(file);
+		if (response.data.resultCode === 0) {
+			dispatch(savePhotoSuccess(response.data.data.photos));
 		}
 	}
 };
